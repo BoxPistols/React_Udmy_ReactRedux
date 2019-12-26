1e3f9821f65e43400b8fa914f859d1b201391a0a
A_ito <BoxPistols@users.noreply.github.com>
2019-12-27 04:18:40 +0900

    component basse

--- src/index.js
+++ src/index.js
@@ -3,18 +3,10 @@ import React from "react";
 import { render } from "react-dom";
 import {} from "./styles.css";
 
-// const returnReactElement = () => {
-//   return <h2>Hello-1</h2>;
-// };
-
-// const returnReactElement2 = () => <h2>Hello-2</h2>;
-
-// const returnReactElement3 = hello3 => <h2>{hello3}</h2>;
-
-const returnMemberInfo = (id, name) => {
-  // const newStrings = id + ":" + name;
-  const newStrings = `${id}番目:${name}さん`;
-  return <h2>{newStrings}</h2>;
+// upper camel case
+const ReturnReactElement = () => {
+  return <h2>name</h2>;
 };
 
-render(returnMemberInfo("#1", "Yamada"), document.getElementById("root"));
+// components <Hoge/>
+render(<ReturnReactElement />, document.getElementById("root"));
