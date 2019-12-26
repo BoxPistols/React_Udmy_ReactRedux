8fef5123dd38ce91e8d7a80d9818bb56a9b481ca
A_ito <BoxPistols@users.noreply.github.com>
2019-12-27 05:03:48 +0900

    React export / include

--- src/index.js
+++ src/index.js
@@ -2,15 +2,5 @@
 import React from "react";
 import { render } from "react-dom";
 import {} from "./styles.css";
-import { var1, func1 } from "./module";
-
-console.log(var1);
-func1();
-
-const App = () => (
-  <div>
-    <h2>Start</h2>
-  </div>
-);
-
-render(<App />, document.getElementById("root"));
+import { ReactComponent } from "./module";
+render(<ReactComponent />, document.getElementById("root"));
--- src/module.js
+++ src/module.js
@@ -1,3 +1,2 @@
-export const var1 = "Strings1";
-
-export const func1 = () => console.log("func1 !!");
+import React from "react";
+export const ReactComponent = () => <h2>ReactComponent</h2>;
