5127428889fe58fa5ed4db4df02f59cb8b62ff39
A_ito <BoxPistols@users.noreply.github.com>
2019-12-27 04:08:56 +0900

    ArrowFunction return

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import React from "react";
 import { render } from "react-dom";
 import {} from "./styles.css";
 
-const returnReactElement = () => {
-  return <h2>Hello</h2>;
+// const returnReactElement = () => {
+//   return <h2>Hello-1</h2>;
+// };
+
+// const returnReactElement2 = () => <h2>Hello-2</h2>;
+
+// const returnReactElement3 = hello3 => <h2>{hello3}</h2>;
+
+const returnMemberInfo = (id, name) => {
+  // const newStrings = id + ":" + name;
+  const newStrings = `${id}番目:${name}さん`;
+  return <h2>{newStrings}</h2>;
 };
 
-render(returnReactElement(), document.getElementById("root"));
+render(returnMemberInfo("#1", "Yamada"), document.getElementById("root"));
