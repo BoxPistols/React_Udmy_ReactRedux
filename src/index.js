5604581fc66b52a4c68fb4ab7c2f0e0b6f904215
A_ito <BoxPistols@users.noreply.github.com>
2019-12-30 17:33:45 +0900

    Set Class

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
-// index.js
-import React from "react";
-import { render } from "react-dom";
-import {} from "./styles.css";
+import React from 'react';
+import { render } from 'react-dom';
+import './styles.css';
 
 class H2O extends React.Component {
   constructor(props) {
     super(props);
     this.state = { temp: 15 };
   }
+
+  H20State(temp) {
+    if (temp <= 0) {
+      return 'ice';
+    } else if (temp >= 100) {
+      return 'steam';
+    } else return 'water';
+  }
   render() {
     return (
-      <div>
-        <h1>{this.props.name}</h1>
-        <h2>{this.state.temp}℃</h2>
-        <button onClick={this.onClickPlus}>+</button>
-        <button onClick={this.onClickPlus10}>+10</button>
-        <button onClick={this.onClickMinus}>-</button>
-        <button onClick={this.onClickMinus10}>-10</button>
+      <div className="box">
+        <div className={this.H20State(this.state.temp)}>
+          <h1>
+            {/* このH20State関数(tempの内容、tempの状態) */}
+            Phase: {this.H20State(this.state.temp)} {this.props.name}
+          </h1>
+          <h2>{this.state.temp}℃</h2>
+          <button onClick={this.onClickMinus10}>-10</button>
+          <button onClick={this.onClickMinus}>-</button>
+          <button onClick={this.onClickPlus}>+</button>
+          <button onClick={this.onClickPlus10}>+10</button>
+        </div>
       </div>
     );
   }
@@ -33,4 +45,4 @@ class H2O extends React.Component {
     this.setState({ temp: this.state.temp - 10 });
   };
 }
-render(<H2O name="温度" />, document.getElementById("root"));
+render(<H2O name="" />, document.getElementById('root'));
